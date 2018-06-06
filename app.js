a69e9d683bb7ac393e78521eaab820bfd94cabdc
hamsemare <30583701+hamsemare@users.noreply.github.com>
2018-06-06 10:19:20 -0600

    First mean stack web application

---
+++ app.js
@@ -0,0 +1,60 @@
+const express= require("express");
+const path= require("path");
+const bodyParser= require("body-parser");
+const cors= require("cors");
+const passport= require("passport");
+const mongoose= require("mongoose");
+
+const config= require("./config/database")
+
+//Connect to the database
+mongoose.connect(config.database);
+
+//// On connection, and log it
+mongoose.connection.on("connected", function(){
+	console.log("connected to the database "+ config.databse);
+});
+
+// On error, and log it
+mongoose.connection.on("error", function(err){
+	console.log("Database error: "+ err);
+});
+
+const app=express();
+
+//Body parser  Middleware
+app.use(bodyParser());
+
+//Set the port
+const port=3000;
+
+const users= require("./routes/users")
+
+//Allows use to access our page from different ports
+app.use(cors());
+
+//Set static folder to public
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./config/passport")(passport);
+
+//use /users for all users accounts
+app.use("/users", users);
+
+//Index Route
+app.get("/", function(req, res){
+	res.send("welcome");
+})
+
+//Start Server
+app.listen(port, function(){
+	console.log("Hello ");
+})
+
+
