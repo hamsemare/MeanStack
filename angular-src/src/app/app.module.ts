192a508be9acc5c3e2df3705e189e75cf379dd5c
hamsemare <30583701+hamsemare@users.noreply.github.com>
2018-06-06 23:46:58 -0600

    progress: just finished the authentication and putting the token to local storage and was able to get the logout function working and clearing the local storage

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from "@angular/router";
+import { FormsModule} from "@angular/forms";
+import { HttpModule} from "@angular/http";
+import {FlashMessagesModule} from "angular2-flash-messages";
+import {AuthService} from "./services/auth.service";
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashBoardComponent } from './components/dash-board/dash-board.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+import {ValidateService} from "./services/validate.service"
+
+const appRoutes: Routes=[
+  {path: "", component : HomeComponent},
+  {path: "register", component : RegisterComponent},
+  {path: "login", component : LoginComponent},
+  {path: "dashboard", component : DashBoardComponent},
+  {path: "profile", component : ProfileComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashBoardComponent,
+    ProfileComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule.forRoot()
   ],
-  providers: [],
+  providers: [ValidateService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
